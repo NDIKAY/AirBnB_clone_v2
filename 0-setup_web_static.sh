4e3c39122abd1a195ee3dbf6f7c9ca79a9b1460a
NDIKUBWIMANA Balthazar <nbarthazar80@gmail.com>
2024-04-25 05:41:20 -0700

    Fabric

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
-
 sudo apt-get update
 sudo apt-get install -y nginx
-sudo ufw allow 'Nginix HTTP'
+sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo bash -c 'echo "<html>
   <head>
   </head>
   <body>
-    Honelton School
+    Holberton School
   </body>
-</html>" | data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html'
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 defaut_server/a location /hbnb_static { alias /data/web_static/current/;' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 defaut_server/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
